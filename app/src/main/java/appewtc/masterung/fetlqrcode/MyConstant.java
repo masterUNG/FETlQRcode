e78d3d03ca3e6aa9977163185f17b2e2391d15e5
masterUNG <phrombutr@gmail.com>
2017-05-17 11:46:11 +0700

    Upload Value To Server Success

---
+++ app/src/main/java/appewtc/masterung/fetlqrcode/MyConstant.java
@@ -0,0 +1,15 @@
+package appewtc.masterung.fetlqrcode;
+
+/**
+ * Created by masterUNG on 5/17/2017 AD.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String urlPostUser = "http://androidthai.in.th/fetl/addUserMaster.php";
+
+    public String getUrlPostUser() {
+        return urlPostUser;
+    }
+}   // Main Class
